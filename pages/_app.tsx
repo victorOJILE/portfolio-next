34b61fdb6af23ef597f83e96eb15671e4941257b
VictorDMN <ojilevictor11@gmail.com>
2026-04-26 03:17:48 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,32 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import localFont from 'next/font/local';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import SocialSidebar from '../components/common/SocialSidebar';
 import BackToTop from '../components/common/BackToTop';
 
+const openSans = localFont({
+  src: '../public/fonts/OpenSans-Regular.ttf',
+  variable: '--font-open-sans',
+  display: 'swap'
+});
+
+const beauty = localFont({
+  src: '../public/fonts/BBeauty-v5.02-648B6363D402E00678B7AAF1D252A5DC.ttf',
+  variable: '--font-beauty',
+  display: 'swap',
+});
+
+const crimson = localFont({
+  src: '../public/fonts/Crimson-Italic.ttf',
+  variable: '--font-crimson',
+  display: 'swap',
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={`${openSans.variable} ${beauty.variable} ${crimson.variable}`}>
       <Header />
       <SocialSidebar />
       <main>
@@ -15,6 +34,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       </main>
       <Footer />
       <BackToTop />
-    </>
+    </div>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://victorojile.com'),
@@ -68,27 +67,9 @@ export const metadata: Metadata = {
   manifest: '/site.webmanifest'
 };
 
-const openSans = localFont({
-  src: '../public/fonts/OpenSans-Regular.ttf',
-  variable: '--font-open-sans',
-  display: 'swap'
-});
-
-const beauty = localFont({
-  src: '../public/fonts/BBeauty-v5.02-648B6363D402E00678B7AAF1D252A5DC.ttf',
-  variable: '--font-beauty',
-  display: 'swap',
-});
-
-const crimson = localFont({
-  src: '../public/fonts/Crimson-Italic.ttf',
-  variable: '--font-crimson',
-  display: 'swap',
-});
-
 export default function Document() {
   return (
-    <Html lang="en" className={`${openSans.variable} ${beauty.variable} ${crimson.variable} scroll-smooth`}>
+    <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
